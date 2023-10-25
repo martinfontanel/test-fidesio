d3cdd1cacf90435d59ac3f55bb59d6c1e721ac6d
Martin FOntanel <martin.fontanel@free.fr>
2023-10-26 00:54:04 +0200

    done

--- home.js
+++ home.js
@@ -1,14 +1,16 @@
 accordion = document.getElementById("accordion").children;
 
 function closeAccordionItem(nbr){
-	for (i=0; i<accordion.length; i++){
-		if (accordion[i].getAttributeNode("aria-level").value == nbr) accordion[i].setAttribute("aria-hidden", true);
-	}
+	/*for (i=0; i<accordion.length; i++){
+		if (accordion[i].getAttributeNode("aria-id").value == nbr) accordion[i].setAttribute("aria-expanded", false);
+	}*/
+	accordion[nbr].setAttribute("aria-expanded", false);
 }
 
 function openAccordionItem(nbr){
-	for (i=0; i<accordion.length; i++){
-		if (accordion[i].getAttributeNode("aria-level").value == nbr) accordion[i].setAttribute("aria-hidden", false);
-	}
+	/*for (i=0; i<accordion.length; i++){
+		if (accordion[i].getAttributeNode("aria-id").value == nbr) accordion[i].setAttribute("aria-expanded", true);
+	}*/
+	accordion[nbr].setAttribute("aria-expanded", true);
 
 }
