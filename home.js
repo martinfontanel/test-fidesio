51551f3619a47be925984f79d45b638b978f4e50
Martin FOntanel <martin.fontanel@free.fr>
2023-10-25 18:55:28 +0200

    accordeon + bannière

--- home.js
+++ home.js
@@ -1,1 +1,14 @@
+accordion = document.getElementById("accordion").children;
 
+function closeAccordionItem(nbr){
+	for (i=0; i<accordion.length; i++){
+		if (accordion[i].getAttributeNode("aria-level").value == nbr) accordion[i].setAttribute("aria-hidden", true);
+	}
+}
+
+function openAccordionItem(nbr){
+	for (i=0; i<accordion.length; i++){
+		if (accordion[i].getAttributeNode("aria-level").value == nbr) accordion[i].setAttribute("aria-hidden", false);
+	}
+
+}
